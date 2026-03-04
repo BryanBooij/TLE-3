87a1ee8abd1197f24aeeb74c98ffd2dc8f1959da
Star <xxxxstardustxx@gmail.com>
2026-03-04 12:59:30 +0100

    Tried adding simple navigation so I can work on the contents of the learning modules page

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {createBrowserRouter, RouterProvider} from "react-router";
+import LeermoduleAdmin from "./LeermoduleAdmin.jsx";
+import Navigation from "./Navigation.jsx";
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+    const router = createBrowserRouter([
+        {
+            element: <Navigation/>,
+            children: [
+                {
+                    //path: "/home",
+                    //element: <Home/>,//either remove or make a home page for admin
+                },
+                {
+                    path: "/leermodule",
+                    element: <LeermoduleAdmin/>
+                },
+                {
+                    //path: "/users",
+                    //element: <UsersAdmin/>, //change this to the right page
+                },
+            ],
+        },
+    ]);
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    function App() {
+        return <RouterProvider router={router}/>;
+    }
 }
 
 export default App
---
+++ src/LeermoduleAdmin.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function LeermoduleAdmin() {
+
+return (
+    <>
+        <h1>Leermodules</h1>
+
+    </>
+)
+}
+
+export default LeermoduleAdmin
