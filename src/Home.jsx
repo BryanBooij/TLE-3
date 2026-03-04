b67a47dc08120887799c5291eff139433a189035
Bryan_1025561 <1025561@hr.nl>
2026-03-04 12:59:32 +0100

    added small button functionality

--- src/Home.jsx
+++ src/Home.jsx
@@ -4,15 +4,22 @@ import ButtonMain from "./buttons/ButtonMain.jsx";
 import ButtonGreen from "./buttons/ButtonGreen.jsx";
 import ButtonFooter from "./buttons/ButtonFooter.jsx";
 import ButtonHeader from "./buttons/ButtonHeader.jsx";
+import {useNavigate} from "react-router";
 
 function Home(){
+    const navigate = useNavigate();
+
+    const handleHomeClick = () => {
+        navigate("*");
+    };
+
     return (
         <>
             <div className="home-container">
                 <h1>Home Page</h1>
                 <p>Welkom op onze homepage</p>
                 {/* Buttons placeholder for not to showcase */}
-                <ButtonMain label="Button Main"/>
+                <ButtonMain label="Button Main" onClick={handleHomeClick}/>
                 <ButtonGreen label="Button Green"/>
                 <ButtonHeader label="Button Header"/>
                 <ButtonFooter label="Button Footer"/>
