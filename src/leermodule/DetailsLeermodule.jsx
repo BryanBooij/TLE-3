37d6b98792bd03cd2dcbacca5a6ce06a3b42119b
Star <xxxxstardustxx@gmail.com>
2026-03-10 14:35:15 +0100

    - Added some more css to neatly align the "data" - Buttons have been added but don't do anything yet

--- src/leermodule/DetailsLeermodule.jsx
+++ src/leermodule/DetailsLeermodule.jsx
@@ -2,6 +2,9 @@ import '../App.css';
 import '../index.css';
 import { useParams } from "react-router-dom";
 import './styleLeermoduleAdmin.css';
+import '../buttons/ButtonPurple.jsx';
+import ButtonPurple from "../buttons/ButtonPurple.jsx";
+import ButtonBlack from "../buttons/ButtonBlack.jsx";
 
 function DetailsLeermodule() {
 
@@ -15,10 +18,11 @@ function DetailsLeermodule() {
             <div id="detailsLeermoduleContainter">
                 <h1>You're now checking out details of leermodule: {result}</h1>
                 {/*div times amount of existing questions*/}
+                <div id="questionsContainer">
                 <div>
                     <h2 className="questionTitle">Question 1</h2>
                     {/*p times the amount of existing answers*/}
-                    <div className="answers-container">
+                    <div className="answers_container">
                         <p>
                             answer
                         </p>
@@ -32,7 +36,7 @@ function DetailsLeermodule() {
                 </div>
                 <div>
                     <h2 className="questionTitle">Question 2</h2>
-                    <div className="answers-container">
+                    <div className="answers_container">
                         <p>
                             answer
                         </p>
@@ -46,7 +50,7 @@ function DetailsLeermodule() {
                 </div>
                 <div>
                     <h2 className="questionTitle">Question 3</h2>
-                    <div className="answers-container">
+                    <div className="answers_container">
                         <p>
                             answer
                         </p>
@@ -56,6 +60,11 @@ function DetailsLeermodule() {
                         <p>
                             answer
                         </p>
+                    </div>
+                </div>
+                    <div className={"buttons_container_details"}>
+                        <ButtonPurple alt={"Edit button"} label={"Edit data"}></ButtonPurple>
+                        <ButtonBlack alt={"Delete button"} label={"Delete leermodule"}></ButtonBlack>
                     </div>
                 </div>
             </div>
