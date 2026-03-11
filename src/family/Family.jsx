2896ffd37b1b6f20a0a5cc2a4bae52e28d8c442a
Bryan_1025561 <1025561@hr.nl>
2026-03-11 13:41:45 +0100

    added family buttons for family page

--- src/family/Family.jsx
+++ src/family/Family.jsx
@@ -1,11 +1,22 @@
 import "./family.css"
+import BigButton from "../buttons/BigButton.jsx";
+import InfoButton from "../buttons/InfoButton.jsx";
+import {useNavigate} from "react-router";
 function Family(){
+    const navigate = useNavigate();
     return (
         <>
             <div className="family-container">
                 <h1>Family AI</h1>
                 <p>Dit is jou familie!</p>
                 <img src="/family-ai-drawing.png" alt="Family AI" className="home-image"/>
+                <h3>Hier kan je de informatie bekijken per familie lid</h3>
+                <div className="family-buttons">
+                    <BigButton alt="Vader" label="Vader" onClick={() => navigate("/Family")}/>
+                    <BigButton alt="Moeder" label="Moeder" onClick={() => navigate("/Family")}/>
+                    <BigButton alt="Zoon" label="Zoon" onClick={() => navigate("/Family")}/>
+                    <BigButton alt="Dochter" label="Dochter" onClick={() => navigate("/Family")}/>
+                </div>
             </div>
         </>
     )
